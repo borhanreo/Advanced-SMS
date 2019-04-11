eed9009d2df01e6e366446cdb77eb93167f08386
borhan <borhan.u.cse@gmail.com>
2019-04-11 18:19:10 +0600

    Start

---
+++ SMSapplication/README.md
@@ -0,0 +1,4 @@
+﻿###
+# Additional information: The 'Microsoft.Jet.OLEDB.4.0' provider is not registered on the local machine.
+# Download https://www.microsoft.com/en-us/download/details.aspx?id=13255
+# Install 
---
+++ SMSapplication/SMSapplication.cs
@@ -0,0 +1,434 @@
+/*
+ * Created by: Syeda Anila Nusrat. 
+ * Date: 1st August 2009
+ * Time: 2:54 PM 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Data.OleDb;
+using System.Collections; 
+namespace SMSapplication
+{
+    public partial class SMSapplication : Form
+    {
+
+        ArrayList arryList1 = new ArrayList();
+        #region Constructor
+        public SMSapplication()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Private Variables
+        SerialPort port = new SerialPort();
+        clsSMS objclsSMS = new clsSMS();
+        ShortMessageCollection objShortMessageCollection = new ShortMessageCollection();
+        #endregion
+
+        #region Private Methods
+
+        #region Write StatusBar
+        private void WriteStatusBar(string status)
+        {
+            try
+            {
+                statusBar1.Text = "Message: " + status;
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+        #endregion
+        
+        #endregion
+
+        #region Private Events
+
+        private void SMSapplication_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                #region Display all available COM Ports
+                string[] ports = SerialPort.GetPortNames();
+
+                // Add all port names to the combo box:
+                foreach (string port in ports)
+                {
+                    this.cboPortName.Items.Add(port);
+                }
+                #endregion
+
+                //Remove tab pages
+                this.tabSMSapplication.TabPages.Remove(tbSendSMS);
+                this.tabSMSapplication.TabPages.Remove(tbReadSMS);
+                this.tabSMSapplication.TabPages.Remove(tbDeleteSMS);
+
+                this.btnDisconnect.Enabled = false;
+            }
+            catch(Exception ex)
+            {
+                ErrorLog(ex.Message);
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Open communication port 
+                this.port = objclsSMS.OpenPort(this.cboPortName.Text, Convert.ToInt32(this.cboBaudRate.Text), Convert.ToInt32(this.cboDataBits.Text), Convert.ToInt32(this.txtReadTimeOut.Text), Convert.ToInt32(this.txtWriteTimeOut.Text));
+
+                if (this.port != null)
+                {
+                    this.gboPortSettings.Enabled = false;
+
+                    //MessageBox.Show("Modem is connected at PORT " + this.cboPortName.Text);
+                    this.statusBar1.Text = "Modem is connected at PORT " + this.cboPortName.Text;
+
+                    //Add tab pages
+                    this.tabSMSapplication.TabPages.Add(tbSendSMS);
+                    this.tabSMSapplication.TabPages.Add(tbReadSMS);
+                    this.tabSMSapplication.TabPages.Add(tbDeleteSMS);
+
+                    this.lblConnectionStatus.Text = "Connected at " + this.cboPortName.Text;
+                    this.btnDisconnect.Enabled = true;
+                }
+
+                else
+                {
+                    //MessageBox.Show("Invalid port settings");
+                    this.statusBar1.Text = "Invalid port settings";
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex.Message);
+            }
+
+        }
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.gboPortSettings.Enabled = true;
+                objclsSMS.ClosePort(this.port);
+
+                //Remove tab pages
+                this.tabSMSapplication.TabPages.Remove(tbSendSMS);
+                this.tabSMSapplication.TabPages.Remove(tbReadSMS);
+                this.tabSMSapplication.TabPages.Remove(tbDeleteSMS);
+
+                this.lblConnectionStatus.Text = "Not Connected";
+                this.btnDisconnect.Enabled = false;
+
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex.Message);
+            }
+        }
+
+        private void btnSendSMS_Click(object sender, EventArgs e)
+        {
+
+            //.............................................. Send SMS ....................................................
+            try
+            {
+
+                if (objclsSMS.sendMsg(this.port, this.txtSIM.Text, this.txtMessage.Text))
+                {
+                    //MessageBox.Show("Message has sent successfully");
+                    this.statusBar1.Text = "Message has sent successfully";
+                }
+                else
+                {
+                    //MessageBox.Show("Failed to send message");
+                    this.statusBar1.Text = "Failed to send message";
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex.Message);
+            }
+        }
+        private void btnReadSMS_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //count SMS 
+                int uCountSMS = objclsSMS.CountSMSmessages(this.port);
+                if (uCountSMS > 0)
+                {
+
+                    #region Command
+                    string strCommand = "AT+CMGL=\"ALL\"";
+
+                    if (this.rbReadAll.Checked)
+                    {
+                        strCommand = "AT+CMGL=\"ALL\"";
+                    }
+                    else if (this.rbReadUnRead.Checked)
+                    {
+                        strCommand = "AT+CMGL=\"REC UNREAD\"";
+                    }
+                    else if (this.rbReadStoreSent.Checked)
+                    {
+                        strCommand = "AT+CMGL=\"STO SENT\"";
+                    }
+                    else if (this.rbReadStoreUnSent.Checked)
+                    {
+                        strCommand = "AT+CMGL=\"STO UNSENT\"";
+                    }
+                    #endregion
+
+                    // If SMS exist then read SMS
+                    #region Read SMS
+                    //.............................................. Read all SMS ....................................................
+                    objShortMessageCollection = objclsSMS.ReadSMS(this.port, strCommand);
+                    foreach (ShortMessage msg in objShortMessageCollection)
+                    {
+
+                        ListViewItem item = new ListViewItem(new string[] { msg.Index, msg.Sent, msg.Sender, msg.Message });
+                        item.Tag = msg;
+                        lvwMessages.Items.Add(item);
+
+                    }
+                    #endregion
+
+                }
+                else
+                {
+                    lvwMessages.Clear();
+                    //MessageBox.Show("There is no message in SIM");
+                    this.statusBar1.Text = "There is no message in SIM";
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex.Message);
+            }
+        }
+        private void btnDeleteSMS_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Count SMS 
+                int uCountSMS = objclsSMS.CountSMSmessages(this.port);
+                if (uCountSMS > 0)
+                {
+                    DialogResult dr = MessageBox.Show("Are u sure u want to delete the SMS?", "Delete confirmation", MessageBoxButtons.YesNo);
+
+                    if (dr.ToString() == "Yes")
+                    {
+                        #region Delete SMS
+
+                        if (this.rbDeleteAllSMS.Checked)
+                        {                           
+                            //...............................................Delete all SMS ....................................................
+
+                            #region Delete all SMS
+                            string strCommand = "AT+CMGD=1,4";
+                            if (objclsSMS.DeleteMsg(this.port, strCommand))
+                            {
+                                //MessageBox.Show("Messages has deleted successfuly ");
+                                this.statusBar1.Text = "Messages has deleted successfuly";
+                            }
+                            else
+                            {
+                                //MessageBox.Show("Failed to delete messages ");
+                                this.statusBar1.Text = "Failed to delete messages";
+                            }
+                            #endregion
+                            
+                        }
+                        else if (this.rbDeleteReadSMS.Checked)
+                        {                          
+                            //...............................................Delete Read SMS ....................................................
+
+                            #region Delete Read SMS
+                            string strCommand = "AT+CMGD=1,3";
+                            if (objclsSMS.DeleteMsg(this.port, strCommand))
+                            {
+                                //MessageBox.Show("Messages has deleted successfuly");
+                                this.statusBar1.Text = "Messages has deleted successfuly";
+                            }
+                            else
+                            {
+                                //MessageBox.Show("Failed to delete messages ");
+                                this.statusBar1.Text = "Failed to delete messages";
+                            }
+                            #endregion
+
+                        }
+
+                        #endregion
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex.Message);
+            }
+
+        }
+        private void btnCountSMS_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Count SMS
+                int uCountSMS = objclsSMS.CountSMSmessages(this.port);
+                this.txtCountSMS.Text = uCountSMS.ToString();
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region Error Log
+        public void ErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+
+            try
+            {
+                WriteStatusBar(Message);
+
+                string sLogFormat = DateTime.Now.ToShortDateString().ToString() + " " + DateTime.Now.ToLongTimeString().ToString() + " ==> ";
+                //string sPathName = @"E:\";
+                string sPathName = @"SMSapplicationErrorLog_";
+
+                string sYear = DateTime.Now.Year.ToString();
+                string sMonth = DateTime.Now.Month.ToString();
+                string sDay = DateTime.Now.Day.ToString();
+
+                string sErrorTime = sDay + "-" + sMonth + "-" + sYear;
+
+                sw = new StreamWriter(sPathName + sErrorTime + ".txt", true);
+
+                sw.WriteLine(sLogFormat + Message);
+                sw.Flush();
+
+            }
+            catch (Exception ex)
+            {
+                //ErrorLog(ex.ToString());
+            }
+            finally
+            {
+                if (sw != null)
+                {
+                    sw.Dispose();
+                    sw.Close();
+                }
+            }
+            
+        }
+        #endregion 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse Excel Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "Excel",
+                Filter = "excel files (*.xlsx)|*.xlsx",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //textBox1.Text = openFileDialog1.FileName;
+                Console.WriteLine(openFileDialog1.FileName);
+                
+                
+                string path = @"E:\sms.xlsx";
+                string con = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + openFileDialog1.FileName + ";Extended Properties=Excel 12.0;";
+                using (OleDbConnection connection = new OleDbConnection(con))
+                {
+                    connection.Open();
+                    OleDbCommand command = new OleDbCommand("select * from [Sheet1$]", connection);
+                    using (OleDbDataReader dr = command.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            var row1Col0 = dr[1];
+                            arryList1.Add(row1Col0);
+                            Console.WriteLine(row1Col0);
+                        }
+                    }
+                }
+                 
+            }
+            Console.WriteLine(arryList1.Count);
+            System.Object[] ItemObject = new System.Object[arryList1.Count];
+            for (int i = 0; i <= arryList1.Count-1; i++)
+            {
+                ItemObject[i] = arryList1[i];
+            }
+            listBox1.Items.AddRange(ItemObject);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Provider=Microsoft.ACE.OLEDB.12.0;
+            //string con =@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=E:\sms.xlsx;" +@"Extended Properties='Excel 8.0;HDR=Yes;'";
+             
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            //.............................................. Send SMS ....................................................
+            try
+            {
+                for (int i = 0; i < arryList1.Count;i++ )
+                {
+                    if (objclsSMS.sendMsg(this.port, "0"+arryList1[i].ToString(), this.txtMessage.Text))
+                    {
+                        //MessageBox.Show("Message has sent successfully");
+                        
+                        this.statusBar1.Text = "Message has sent successfully "+(i+1);
+                    }
+                    else
+                    {
+                        //MessageBox.Show("Failed to send message");
+                        this.statusBar1.Text = "Failed to send message ";
+                    }
+                }
+                    
+
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex.Message);
+            }
+        }
+    
+    }
+}
